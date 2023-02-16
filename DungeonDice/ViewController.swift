ea8d14d6cf779612dbe3d27a8de279c523033985
Glenda Mullan <glenda.mullan@gmail.com>
2023-02-16 13:13:40 +1100

    First programming save

--- DungeonDice/ViewController.swift
+++ DungeonDice/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var AppName: UILabel!
+    
+    @IBOutlet weak var ResultsMessageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func FourSidesButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func SixSidesButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func EightSidesButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func TenSidesButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func TwelveSidesButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func TwentySidesButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func HundredSidesButton(_ sender: UIButton) {
+    }
+    
 }
 
